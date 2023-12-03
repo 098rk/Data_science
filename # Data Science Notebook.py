4099877696b5b12862d122e627c15b8f39e785d9
098rk <91719345+098rk@users.noreply.github.com>
2023-12-03 13:51:10 +0100

    Add files via upload

---
+++ # Data Science Notebook.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Data Science Notebook
+
+
+# In[3]:
+
+
+# - Python
+# - R
+# - Julia
+
+
+# In[4]:
+
+
+# | Tool       | Purpose                |
+# |------------|------------------------|
+# | Pandas     | Data manipulation      |
+# | Scikit-learn | Machine learning      |
+
+
+# In[5]:
+
+
+# Example
+a = 5
+b = 10
+result_multiply = a * b
+result_add = a + b
+result_multiply, result_add
+
+
+# In[6]:
+
+
+# Example
+minutes = 120
+hours = minutes / 60
+hours
+
+
+# In[7]:
+
+
+# Author: Ruby Khan
+
+
+# In[ ]:
+
+
+
+
